b2c6cda17be4bfecd956e109ad16c6afd67bc9cb
Daniel Marcos <danielmarcos@MacBook-Air-de-Daniel.local>
2021-09-13 21:24:56 +0200

    first commit setup and homepage

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components'
+import Particle from '../Components/Particle';
+import GithubIcon from '@material-ui/icons/GitHub';
+
+export default function HomePage() {
+    return (
+        <HomePageStyled>
+            <div className="particle-con">
+                <Particle />
+            </div>
+            <div className="typography">
+                <h1>Hi, I'm <span>Daniel Marcos</span></h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Atque nihil voluptates ea dolore vel 
+                    repellat? Quia tenetur non quam exercitationem. Lorem ipsum dolor sit amet.
+                </p>
+                <div className="icons">
+                    <a href="https://github.com/dmarcosc" target="_blank" className="icon i-github" rel="noreferrer">
+                        <GithubIcon />
+                    </a>
+                </div>
+            </div>
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+   
+   .typography{
+       position: absolute;
+       top: 50%;
+       left: 50%;
+       transform: translate(-50%, -50%);
+       text-align: center;
+       width: 80%;
+       .icons{
+           display: flex;
+           justify-content: center;
+           margin-top: 1rem;
+           .icon{
+               border: 2px solid var(--border-color);
+               display: flex;
+               align-items: center;
+               justify-content: center;
+               border-radius: 50%;
+               transition: all .4s ease-in-out;
+               cursor: pointer;
+               &:hover{
+                   border: 2px solid var(--primary-color);
+                   color: var(--primary-color);
+               }
+               &:not(:last-child){
+                   margin-right: 1rem;
+               }
+               svg{
+                   margin: .5rem;
+               }
+           }
+           .i-github{
+               &:hover{
+                   border: 2px solid #5F4687;
+                   color: #5F4687;
+               }
+           }
+       }
+   }
+`;
